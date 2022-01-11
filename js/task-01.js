ba57a613e455781dd5f1b83daea74ad3aa234ce1
Christian Duda <duda.kristian.ns@gmail.com>
2022-01-11 11:53:20 +0200

    Update task-01.js
    first try

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const categoriesEl = document.querySelector("#categories");
+const itemsEl = categoriesEl.children;
+console.log(itemsEl);
+const itemTitleEl = document.querySelectorAll(".item");
+console.log(itemTitleEl);
+const task1 = `Number of categories: ${itemsEl.length}`;
+
+console.log(task1);
