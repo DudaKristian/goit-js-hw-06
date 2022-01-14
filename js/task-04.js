d5c68cb8afa79fb2dff3187d84911ab525ce7c30
Christian Duda <duda.kristian.ns@gmail.com>
2022-01-14 15:50:53 +0200

    Update task-04.js
    variable rename

--- js/task-04.js
+++ js/task-04.js
@@ -1,15 +1,15 @@
 const decrementEl = document.querySelector('button[data-action="decrement"]');
 const incrementEl = document.querySelector('button[data-action="increment"]');
-const valueEl = document.querySelector("#value");
+const counterValue = document.querySelector("#value");
 
-let number = Number(valueEl.textContent);
+let number = Number(counterValue.textContent);
 
 const addToValue = () => {
-  valueEl.textContent = number += 1;
+  counterValue.textContent = number += 1;
 };
 
 const removeFromValue = () => {
-  valueEl.textContent = number -= 1;
+  counterValue.textContent = number -= 1;
 };
 
 incrementEl.addEventListener("click", addToValue);
