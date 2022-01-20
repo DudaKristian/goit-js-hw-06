c80ae9fb91920868b5782478457f1043c517ce68
Christian Duda <duda.kristian.ns@gmail.com>
2022-01-20 16:17:22 +0200

    Update task-05.js
    textContent bug fixed

--- js/task-05.js
+++ js/task-05.js
@@ -2,5 +2,9 @@ const inputEl = document.querySelector("#name-input");
 const outputEl = document.querySelector("#name-output");
 
 inputEl.addEventListener("input", (event) => {
-  outputEl.textContent = event.currentTarget.value;
+  if (event.currentTarget.value === "") {
+    outputEl.textContent = "Anonymous";
+  } else {
+    outputEl.textContent = event.currentTarget.value;
+  }
 });
